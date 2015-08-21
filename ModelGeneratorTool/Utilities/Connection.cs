dc7ca1a7cb40383def49193c6fb42594fec22c28
GopikiranLakkoju <gopimech2030@gmail.com>
2015-08-22 01:28:05 +0530

    Comments for connection class

--- ModelGeneratorTool/Utilities/Connection.cs
+++ ModelGeneratorTool/Utilities/Connection.cs
@@ -5,8 +5,17 @@
     /// </summary>
     internal sealed class Connection
     {
+        /// <summary>
+        /// Gets or sets DataSource
+        /// </summary>
         public string DataSource { get; set; }
+        /// <summary>
+        /// Gets or sets Database
+        /// </summary>
         public string Database { get; set; }
+        /// <summary>
+        /// Gets or sets Schema
+        /// </summary>
         public string Schema { get; set; }
     }
 }
